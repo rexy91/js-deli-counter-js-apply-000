a16364f559f103f92ec74c16c399b49d7fa33bd2
Yu Hao Ye <rxye1991@gmail.com>
2019-09-12 14:27:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function takeANumber(array, person){
+    array.push(person)
+
+    return `Welcome, ${person}. You are number ${array.length} in line.`
+
+}
+
+function nowServing(array){
+  if (array.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    
+    return `Currently serving ${array.shift()}.`
+  }
+}
+
+function currentLine(array){
+  if (array.length === 0){
+    return "The line is currently empty."
+  }
+  else{
+    var empty_array = []
+    for (var i=0; i<array.length; i++){
+      empty_array.push(` `+ [i+1]+`. `  + array[i])
+    }
+    return 'The line is currently:' + empty_array 
+  }
+}
